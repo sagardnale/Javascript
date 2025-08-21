de0c10ee75d14baa4518d3667ec1fa7d3f0e8cae
sagardnale <nalesagard@gmail.com>
2025-08-21 10:10:46 +0530

    Reverse String using split,reverse,join function

--- String_Program/Easy/ReverseString.js
+++ String_Program/Easy/ReverseString.js
@@ -6,5 +6,11 @@ function reverseString(str){
     }
     return reverseStr;
 } 
+console.log(reverseString("Welcome to india"));
+
+// Reverse String using split,reverse and join function.
+function reverseString(str){
+    return str.split('').reverse().join('');
+} 
 
 console.log(reverseString("Welcome to india"));
