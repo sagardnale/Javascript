14cffc82c949cadef80e653a5e312f0e833f925b
sagardnale <nalesagard@gmail.com>
2025-08-21 10:35:51 +0530

    check string is palindrome using for loop

---
+++ String_Program/Easy/PalindromeString.js
@@ -0,0 +1,11 @@
+//check string is palindrome using for loop
+function isPalindromeString(str){
+    for(let i=0,j=str.length-1;i<j && j>i;i++,j--){
+        if(str[i] != str[j]){
+            return false;
+        }
+    }
+    return true;
+} 
+console.log(isPalindromeString("nitin"));
+console.log(isPalindromeString("Sagar"));
