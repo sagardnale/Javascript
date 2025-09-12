c002796fd09c716e1a018bd3ba3d092898c31fc2
sagardnale <nalesagard@gmail.com>
2025-09-12 10:17:34 +0530

    created function to find substring from string

---
+++ String_Program/Easy/FindSubString.js
@@ -0,0 +1,6 @@
+function findSubString(str,subStr){
+    return str.includes(subStr);
+}
+
+console.log(findSubString('I am sagar nale','nale'));
+console.log(findSubString('I am sagar nale','hello'));
