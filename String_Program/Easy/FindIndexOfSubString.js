56ce97eaf4bf3721cc6fa7d252f49ccd064de820
sagardnale <nalesagard@gmail.com>
2025-09-12 10:28:41 +0530

    created function to find index of substring

---
+++ String_Program/Easy/FindIndexOfSubString.js
@@ -0,0 +1,6 @@
+function findIndexOfSubString(str,subStr){
+    return str.indexOf(subStr);
+}
+
+console.log(findIndexOfSubString('I am sagar nale','nale'));
+console.log(findIndexOfSubString('I am sagar nale','welcome'));
